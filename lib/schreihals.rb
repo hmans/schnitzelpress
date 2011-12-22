8c35ea9c1e64e61152a2e067b3c87b29db441f72
Hendrik Mans <hendrik@mans.de>
2011-12-22 19:25:42 +0100

    Clean up schreihals.rb

--- lib/schreihals.rb
+++ lib/schreihals.rb
@@ -3,6 +3,10 @@ require 'sinatra'
 require 'document_mapper'
 
 module Schreihals
+  class Post
+    include DocumentMapper::Document
+    self.directory = 'posts'
+  end
 
   class App < Sinatra::Application
     get '/' do
@@ -17,37 +21,5 @@ module Schreihals
         "not found :("
       end
     end
-  end
-
-  # class App < Renee::Application
-  #   app {
-  #     path('/').get.foo!
-  #     path('posts') do
-  #       var do |slug|
-  #         if post = Post.where(:slug => slug).first
-  #           halt post.title
-  #         else
-  #           halt "not found :("
-  #         end
-  #       end
-  #     end
-  #   }.setup {
-  #     views_path "#{Gem.loaded_specs['schreihals'].full_gem_path}/views"
-  #   }
-
-  #   def foo!
-  #     render! "index.haml" #view, :layout => "layouts/app", :locals => {:title_part => title }
-  #   end
-
-  #   def show_post!
-  #     var :string do |slug|
-  #       halt "post! #{slug}"
-  #     end
-  #   end
-  # end
-
-  class Post
-    include DocumentMapper::Document
-    self.directory = 'posts'
   end
 end
