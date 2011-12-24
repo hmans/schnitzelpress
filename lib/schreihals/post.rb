9ca59d07c2d77eeb8c1bbc2e104b23a9c33b0a82
Hendrik Mans <hendrik@mans.de>
2011-12-24 21:35:34 +0100

    Move App and Post classes into their own .rb files.

---
+++ lib/schreihals/post.rb
@@ -0,0 +1,50 @@
+module Schreihals
+  class Post
+    include DocumentMapper::Document
+
+    def after_load
+      # Set some defaults
+      #
+      self.attributes = {
+        disqus: true,
+        status: 'published',
+        summary: nil,
+        link: nil
+      }.merge(attributes)
+
+      # Set slug
+      #
+      if !attributes.has_key? :slug
+        begin
+          match = attributes[:file_name_without_extension].match(/(\d{4}-\d{1,2}-\d{1,2}[-_])?(.*)/)
+          attributes[:slug] = match[2]
+        rescue NoMethodError => err
+        end
+      end
+    end
+
+    def to_url
+      date.present? ? "/#{year}/#{month}/#{day}/#{slug}/" : "/#{slug}/"
+    end
+
+    def disqus_identifier
+      attributes[:disqus_identifier] || file_name_without_extension
+    end
+
+    def disqus?
+      disqus && published?
+    end
+
+    def published?
+      status == 'published'
+    end
+
+    def post?
+      date.present?
+    end
+
+    def page?
+      !post?
+    end
+  end
+end
