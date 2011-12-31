7da71dc1b3077e85eada0a6be6b830c257092766
Hendrik Mans <hendrik@mans.de>
2011-12-31 01:19:21 +0100

    More tests.

--- test/app_test.rb
+++ test/app_test.rb
@@ -7,11 +7,22 @@ end
 context Schreihals::App do
   setup { Rack::MockRequest.new(MyTestApp.new) }
 
-  context "GET /" do
+  context "when loading the home page" do
     setup { topic.get '/' }
+
     asserts(:status).equals 200
-    asserts(:body).present?
+    asserts(:body).present
     asserts(:content_type).equals 'text/html;charset=utf-8'
     asserts(:body).includes_elements("section.posts article.post", 2)
+  end
+
+  context "when loading a specific post's page" do
+    setup { topic.get '/2011/12/23/first-post/' }
+
+    asserts(:status).equals 200
+    asserts(:body).present
+    asserts(:content_type).equals 'text/html;charset=utf-8'
+    asserts(:body).includes_elements("section.post article.post", 1)
+    asserts(:body).includes_html('section.post article.post header h2 a' => "First Post\.")
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,29 +12,42 @@ require 'rack/test'
 set :environment, :test
 
 module Riot
-  # class IncludesHTMLMacro < Riot::AssertionMacro
-  #   register :includes_html
+  class PresentMacro < Riot::AssertionMacro
+    register :present
 
-  #   def evaluate(actual, expected)
-  #     doc = Nokogiri::HTML.new(actual)
-  #     expected = expected.to_a.flatten
+    def evaluate(thing)
+      thing.present? ? pass("is present") : fail("expected to be present")
+    end
 
-  #     if (doc/expected.first).empty?
-  #       fail("expected #{actual} to contain a <#{expected.first}>")
-  #     elsif !(doc/expected.first).inner_html.match(expected.last)
-  #       fail("expected <#{expected.first}> to contain #{expected.last}")
-  #     else
-  #       pass
-  #     end
-  #   end
-  # end
+    def devaluate(thing)
+      thing.present? ? fail("expected to not be present") : pass("is not present")
+    end
+  end
+
+  class IncludesHTMLMacro < Riot::AssertionMacro
+    register :includes_html
+
+    def evaluate(actual, expected)
+      doc = Nokogiri::HTML(actual)
+      expected = expected.to_a.flatten
+
+      if (doc/expected.first).empty?
+        fail("expected #{actual} to contain a <#{expected.first}>")
+      elsif !(doc/expected.first).inner_html.match(expected.last)
+        fail("expected <#{expected.first}> to contain #{expected.last}")
+      else
+        pass "contains <#{expected.first}> with \"#{expected.last}\""
+      end
+    end
+  end
 
   class IncludesElementsMacro < Riot::AssertionMacro
     register :includes_elements
 
     def evaluate(actual, selector, count)
       doc = Nokogiri::HTML(actual)
-      (doc/selector).size == count ? pass : fail("expected #{actual} to contain #{count} #{selector}(s)")
+      (doc/selector).size == count ? pass("contains #{count} instance#{"s" unless count == 1} of <#{selector}>") :
+        fail("expected #{actual} to contain #{count} #{selector}(s)")
     end
   end
 
