9a7f4a6c23053cd4d2917cc8e41695277aff3c47
Hendrik Mans <hendrik@mans.de>
2011-12-31 01:48:10 +0100

    Test 404s and static files.

--- test/app_test.rb
+++ test/app_test.rb
@@ -49,4 +49,20 @@ context Schreihals::App do
     asserts(:content_type).equals 'application/xml+atom'
     asserts(:body).includes_elements("entry", 2)
   end
+
+  context "when loading a static asset provided by schreihals" do
+    setup { topic.get '/favicon.ico' }
+
+    asserts(:status).equals 200
+    asserts(:body).present
+    asserts(:content_type).equals 'image/vnd.microsoft.icon'
+  end
+
+  context "when loading an invalid URL" do
+    setup { topic.get '/ooga/booga/foo/to/the/bar' }
+
+    asserts(:status).equals 404
+    asserts(:body).present
+    asserts(:content_type).equals 'text/html;charset=utf-8'
+  end
 end
