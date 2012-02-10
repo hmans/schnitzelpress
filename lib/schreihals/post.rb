eb46561e05d5a79895997821e923495dc7fa8d14
Hendrik Mans <hendrik@mans.de>
2012-02-10 20:20:39 +0100

    Fix Post model

--- lib/schreihals/post.rb
+++ lib/schreihals/post.rb
@@ -4,73 +4,72 @@ module Schreihals
   class Post
     include Mongoid::Document
 
-    # def initialize(*args)
-    #   super
-    #   self.attributes = {
-    #     'disqus'    => true,
-    #     'status'    => 'published',
-    #     'summary'   => nil,
-    #     'link'      => nil,
-    #     'read_more' => nil,
-    #     'date'      => nil,
-    #     'title'     => nil,
-    #     'slug'      => nil,
-    #     'disqus_identifier' => file_name
-    #   }.merge(attributes)
+    # basic data
+    field :title, type: String
+    field :body,  type: String
+    field :slugs, type: Array, default: []
 
-    #   # extract date and slug from file name, if possible
-    #   if file_name_without_extension =~ /^(\d{4}-\d{1,2}-\d{1,2})-?(.+)$/
-    #     attributes['date'] ||= Date.parse($1)
-    #     attributes['slug'] ||= $2
-    #   else
-    #     attributes['slug'] ||= file_name_without_extension
-    #   end
-    # end
+    # optional fields
+    field :summary,   type: String
+    field :link,      type: String
+    field :read_more, type: String
 
-  #   def year
-  #     date.year
-  #   end
+    # times & status
+    field :published_at, type: DateTime
+    field :status,       type: Symbol, default: :draft
 
-  #   def month
-  #     date.month
-  #   end
+    # flags
+    field :disqus, type: Boolean, default: false
 
-  #   def day
-  #     date.day
-  #   end
+    validates_presence_of :title, :body, :status
+    validates_inclusion_of :status, in: [:draft, :published]
 
-  #   def to_url
-  #     date.present? ? "/#{year}/#{month}/#{day}/#{slug}/" : "/#{slug}/"
-  #   end
+    def disqus_identifier
+      slug
+    end
 
-  #   def disqus?
-  #     disqus && published?
-  #   end
+    def slug
+      slugs.try(:last)
+    end
 
-  #   def published?
-  #     status == 'published'
-  #   end
+    def to_html
+      Tilt.new("md") { body }.render
+    end
 
-  #   def post?
-  #     date.present?
-  #   end
+    def post?
+      published_at.present?
+    end
 
-  #   def page?
-  #     !post?
-  #   end
+    def page?
+      !post?
+    end
 
-  #   class << self
-  #     def latest(options = {})
-  #       options = {published_only: false}.merge(options)
+    def published?
+      status == :published
+    end
 
-  #       posts = documents.select(&:date)
-  #       posts = posts.select(&:published?) if options[:published_only]
-  #       posts.sort_by(&:date).reverse.first(10)
-  #     end
+    def draft?
+      status == :draft
+    end
 
-  #     def with_slug(slug)
-  #       documents.detect { |p| p.slug == slug }
-  #     end
-  #   end
+    def year
+      published_at.year
+    end
+
+    def month
+      published_at.month
+    end
+
+    def day
+      published_at.day
+    end
+
+    def to_url
+      published_at.present? ? "/#{year}/#{month}/#{day}/#{slug}/" : "/#{slug}/"
+    end
+
+    def disqus?
+      disqus && published?
+    end
   end
 end
