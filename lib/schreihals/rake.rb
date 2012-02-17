bd2b45352e65fb873d0af9dd9d1e48cfedbad8b6
Hendrik Mans <hendrik@mans.de>
2012-02-17 19:06:09 +0100

    Add db:pull rake command

--- lib/schreihals/rake.rb
+++ lib/schreihals/rake.rb
@@ -1,13 +1,21 @@
-task :environment do
-  require 'schreihals'
-end
+require 'schreihals'
 
 desc 'Run the Schreihals console'
-task :console => :environment do
+task :console do
   require 'irb'
   require 'wirble'
   ARGV.clear
   Wirble.init
   Wirble.colorize
   IRB.start
+end
+
+namespace :db do
+  desc 'Import Heroku database to local database'
+  task :pull do
+    system 'MONGO_URL="mongodb://localhost/schreihals" heroku mongo:pull'
+  end
+
+  task :push do
+  end
 end
