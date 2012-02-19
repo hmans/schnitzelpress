293ec293dbca53a43bb4b2910079320fc4945225
Hendrik Mans <hendrik@mans.de>
2012-02-19 02:40:40 +0100

    Once again redirect to post pages after saving posts.

--- lib/schnitzelpress/actions/admin.rb
+++ lib/schnitzelpress/actions/admin.rb
@@ -23,7 +23,7 @@ module SchnitzelPress
         post '/admin/new/?' do
           @post = Post.new(params[:post])
           if @post.save
-            redirect '/admin'
+            redirect url_for(@post)
           else
             haml :'admin/new'
           end
@@ -38,7 +38,7 @@ module SchnitzelPress
           @post = Post.find(params[:id])
           @post.attributes = params[:post]
           if @post.save
-            redirect '/admin'
+            redirect url_for(@post)
           else
             haml :'admin/edit'
           end
