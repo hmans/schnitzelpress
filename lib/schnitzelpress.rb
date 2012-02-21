9a7e6a8d13908dc3c58052cf3e17fa11967d19cf
Hendrik Mans <hendrik@mans.de>
2012-02-21 18:52:21 +0100

    Move SchnitzelPress::MarkdownRenderer into schnitzelpress.rb for now.

--- lib/schnitzelpress.rb
+++ lib/schnitzelpress.rb
@@ -21,6 +21,20 @@ Sass::Engine::DEFAULT_OPTIONS[:load_paths].unshift(File.expand_path("./views"))
 Mongoid.logger.level = 3
 
 module SchnitzelPress
+  class MarkdownRenderer < Redcarpet::Render::HTML
+    include Redcarpet::Render::SmartyPants
+
+    def block_code(code, language)
+      CodeRay.highlight(code, language)
+    end
+
+    def autolink(link, type)
+      OEmbed::Providers.get(link).html
+    rescue OEmbed::NotFound
+      %q(<a href="%s">%s</a>) % [link, link]
+    end
+  end
+
   mattr_reader :mongo_uri
 
   def self.mongo_uri=(uri)
--- lib/schnitzelpress/post.rb
+++ lib/schnitzelpress/post.rb
@@ -8,20 +8,6 @@ SoundCloudProvider << "http://*.soundcloud.com/*"
 OEmbed::Providers.register(SoundCloudProvider)
 
 module SchnitzelPress
-  class MarkdownRenderer < Redcarpet::Render::HTML
-    include Redcarpet::Render::SmartyPants
-
-    def block_code(code, language)
-      CodeRay.highlight(code, language)
-    end
-
-    def autolink(link, type)
-      OEmbed::Providers.get(link).html
-    rescue OEmbed::NotFound
-      %q(<a href="%s">%s</a>) % [link, link]
-    end
-  end
-
   class Post
     include Mongoid::Document
     store_in :posts
