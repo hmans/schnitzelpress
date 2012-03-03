d6cc5366dc80517178d9bc6a88b83f593028b58b
Hendrik Mans <hendrik@mans.de>
2012-03-03 18:19:36 +0100

    Include OEmbed provider name in embed DIVs.

--- lib/schnitzelpress/markdown_renderer.rb
+++ lib/schnitzelpress/markdown_renderer.rb
@@ -8,7 +8,7 @@ module Schnitzelpress
 
     def image(link, title, alt_text)
       oembed = OEmbed::Providers.get(link)
-      %q(<div class="embedded %s">%s</div>) % [oembed.type, oembed.html]
+      %q(<div class="embedded %s %s">%s</div>) % [oembed.type, oembed.provider_name.parameterize, oembed.html]
     rescue OEmbed::NotFound
       %q(<img src="%s" title="%s" alt="%s"/>) % [link, escape_html(title), escape_html(alt_text)]
     end
